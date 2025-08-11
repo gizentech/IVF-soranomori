b35398ccb78c362b6ca23f9cdf089ced4e923f2a
ryo <shiraishi.healthcare@gmail.com>
2025-08-12 08:29:39 +0900

    golf_最新

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>第23回日本生殖看護学会学術集会 - 空の森クリニック見学ツアー</title>
+        <title>空の森クリニック見学ツアー</title>
         <meta name="description" content="第23回日本生殖看護学会学術集会 空の森クリニック見学ツアーの申し込みサイトです。" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
